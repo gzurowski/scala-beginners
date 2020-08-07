2c39ce5fffee3a783084bcad79b10a50bd8e013d
Gregor Zurowski <gregor@zurowski.net>
2020-08-07 10:32:29 +0200

    Add exercises for section 3, lecture 22
    Exceptions

---
+++ src/lectures/part2oop/Exceptions.scala
@@ -0,0 +1,98 @@
+package lectures.part2oop
+
+object Exceptions extends App {
+  val x: String = null
+  // println(x.length)
+  // ^^ this will crash with a NPE
+
+  // 1. throwing exceptions
+  //val aWeirdValue: String = throw new NullPointerException
+
+  // throwable classes extend the Throwable class
+  // Exception and Error are the major Throwable subtypes
+
+  // 2. catching exceptions
+  def getInt(withException: Boolean): Int =
+    if (withException) throw new RuntimeException("No int for you")
+    else 42
+
+  val potentialFail = try {
+    // code that might throw exception
+    getInt(true)
+  } catch {
+    case e: RuntimeException => 43
+  } finally {
+    // code will get executed NO MATTER WHAT
+    // optional
+    // does not influence the return type of this expression!
+    // use finally only for side effects
+    println("finally")
+  }
+
+  println(potentialFail)
+
+  // 3. how to define your own exceptions
+  class MyException extends Exception
+  val exception = new MyException
+  //throw exception
+
+  /*
+  Exercises:
+  1. Crash program with an OutOfMemoryError
+  2. Crash program with a StackOverflowError
+  3. PocketCalculator class
+     - add(x, y)
+     - subtract(x, y)
+     - multiply(x, y)
+     - divide(x, y)
+
+     Throw:
+     - OverflowException if add(x,y) exceeds Int.MAX_VALUE
+     - UnderflowException if subtract(x,y) exceeds Int.MIN_VALUE
+     - MathCalculationException for division by 0
+   */
+
+  // 1. OOM
+  // val array = Array.ofDim(Int.MaxValue)
+
+  // 2. SO
+  // def infinite: Int = 1 + infinite
+  // val noLimit = infinite
+
+  // 3. PocketCalculator
+  class OverflowException extends RuntimeException
+  class UnderflowException extends RuntimeException
+  class MathCalculationException extends RuntimeException("Division by 0")
+  object PocketCalculator {
+    def add(x: Int, y: Int) = {
+      val result = x + y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def subtract(x: Int, y: Int) = {
+      val result = x - y
+      if (x > 0 && y < 0 && result < 0) throw new OverflowException
+      if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def multiply(x: Int, y: Int) = {
+      val result = x * y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x > 0 && y < 0 && result > 0) throw new UnderflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+
+    def divide(x: Int, y: Int) = {
+      if (y == 0) throw new MathCalculationException
+      else x / y
+    }
+  }
+
+  //println(PocketCalculator.add(Int.MaxValue, 10))
+  println(PocketCalculator.divide(2, 0))
+}
